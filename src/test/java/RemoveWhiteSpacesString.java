b557702817d8bb3869f8ceb8cbf63a432e863f85
ashimajain <ashimajain@IN-5CD5127Q9H>
2025-07-09 11:33:26 +0530

    First program- Remove White Spacew From String

---
+++ src/test/java/RemoveWhiteSpacesString.java
@@ -0,0 +1,27 @@
+
+public class RemoveWhiteSpacesString {
+
+	public static void main(String[] args) {
+
+
+
+		String input= "   hello   world    ";
+		System.out.println(input);
+		String str = input.trim(); // trim will remove white spaces from starting & ending of the string
+		
+		System.out.println(str);
+		
+		//to remove white spaces within two words we will use replaceAll method
+		
+		//s= means single white space
+		
+		//s+ = more than 1 white space
+		//now we will use replaceAll method to remove the white spaces within the words
+		
+		str= str.replaceAll("\\s+", " ");
+		System.out.println(str);
+		
+	}
+
+	}
+
